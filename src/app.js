28281bdd92c89795d4d9612e348734525fdff5c0
heresson <heresson@mppa.mp.br>
2021-04-28 09:29:59 -0300

    M1#A12-Renderizando componentes com classes ES6

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 'use strict'
 
-import React from 'react'
+import React, {Component} from 'react'
 import Title from './title'
 
-var App = React.createClass({
-  render: function () {
+class App extends Component {
+  render () {
     return (
       <div className='container'>
-        <Title name='Heresson' lastname='Mendes' />
+        <Title name='Heresson' />
       </div>
     )
   }
-})
+}
 
 export default App
--- src/title.js
+++ src/title.js
@@ -2,28 +2,13 @@
 
 import React from 'react'
 
-const Title = ( {name, lastname} ) => (
-  <h1>Olá {`${name} ${lastname}!`}</h1>
+const Title = ({name, lastname}) => (
+  <h1>Olá {`${name} ${lastname}`}</h1>
 )
 
 Title.defaultProps = {
   name: 'Desconhecido',
   lastname: 'Sem Sobrenome'
 }
-
-// const Title = React.createClass({
-//   getDefaultProps: function () {
-//     return {
-//       name: 'Desconhecido',
-//       lastname: 'Sem Sobrenome'
-//     }
-//   },
-
-//   render: function () {
-//     return (
-//       <h1>Olá {this.props.name + ' ' + this.props.lastname}</h1>
-//     )
-//   }
-// })
 
 export default Title
